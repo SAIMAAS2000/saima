cb3c366927737b51a5e9fb726be8347e9338f24f
SAIMAAS2000 <35587797+SAIMAAS2000@users.noreply.github.com>
2018-03-09 08:59:57 +0530

    Add files via upload

---
+++ def!!!.py
@@ -0,0 +1,22 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: /home/ec2017b213/def_!!.py ====================
+>>> 
+==================== RESTART: /home/ec2017b213/def_!!.py ====================
+>>> 
+==================== RESTART: /home/ec2017b213/def_!!.py ====================
+>>> 
+==================== RESTART: /home/ec2017b213/def_!!.py ====================
+Enter your name: saima
+Hello saima
+>>> 
+==================== RESTART: /home/ec2017b213/def_!!.py ====================
+Enter your name: else
+Hello else
+>>> 
+==================== RESTART: /home/ec2017b213/def_!!.py ====================
+Enter your name: 
+Hello World
+>>> 
