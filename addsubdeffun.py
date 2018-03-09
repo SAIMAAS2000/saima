85e58e37f231933c2d85120e114d41137f00b7f2
SAIMAAS2000 <35587797+SAIMAAS2000@users.noreply.github.com>
2018-03-09 09:00:27 +0530

    Add files via upload

---
+++ addsubdeffun.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    print(a+b)
+    print("addition sucess")
+def sub(x,y):
+    print("sucess")
+    return x-y
+
+
+
+a=100
+b=300
+print("sub of 2 numbers",syb(a,b), "add of 2 numbers",add(a,b))
